a7eb3a6cb4938b2b9cce96f86263689162fbc3f8
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-02 00:57:57 +0200

    refactor zip

--- src/zip/index.d.ts
+++ src/zip/index.d.ts
@@ -1,24 +1,32 @@
-import { Acc, Dec, Reverse, Unshift, Tail, Head, Size } from '..';
+import { Acc, Reverse, Unshift, Tail, Head, IsEqual } from '..';
 
-type GetAllAt<
+type GetAtIndex<
   B extends Array<Array<any>>,
   C extends number,
   R extends Array<number> = []
 > = {
   0: Reverse<R>;
   // @ts-ignore
-  1: GetAllAt<Tail<B>, C, Unshift<R, Head<B>[C]>>;
+  1: GetAtIndex<Tail<B>, C, Unshift<R, Head<B>[C]>>;
 }[B extends [] ? 0 : 1];
 
+type AllEqual<T extends Array<any>, E> = T extends []
+  ? true
+  : {
+      0: AllEqual<Tail<T>, E>;
+      1: false;
+    }[IsEqual<Head<T>, E> extends true ? 0 : 1];
+
+// @ts-ignore
 export type Zip<
   T extends Array<Array<any>>,
-  N extends number = Size<T>,
-  R extends Array<Array<any>> = []
+  N extends number = 0,
+  R extends Array<Array<any>> = [],
+  F extends Array<any> = GetAtIndex<T, N>
 > = T extends []
   ? []
   : {
-      0: R;
-      1: Zip<T, Dec<N>, Unshift<R, GetAllAt<T, Dec<N>>>>;
-    }[N extends 0 ? 0 : 1];
-
-type R = Zip<[[1, 2, 3], [4, 5, 6]]>;
+      0: Reverse<R>;
+      // @ts-ignore
+      1: Zip<T, Acc<N>, Unshift<R, GetAtIndex<T, N>>>;
+    }[AllEqual<F, undefined> extends true ? 0 : 1];
--- src/zip/index.test-d.ts
+++ src/zip/index.test-d.ts
@@ -1,10 +1,18 @@
 import { expectType } from 'tsd';
 import { Zip } from '.';
 
-expectType<Zip<[]>>([]);
-expectType<Zip<[[1]]>>([]);
-expectType<Zip<[]>>([]);
-expectType<Zip<[]>>([]);
-expectType<Zip<[]>>([]);
-expectType<Zip<[]>>([]);
-expectType<Zip<[]>>([]);
+expectType<Zip<[[]]>>([]);
+expectType<Zip<[[], []]>>([]);
+expectType<Zip<[['barney', 'fred'], [36, 40]]>>([
+  ['barney', 36],
+  ['fred', 40],
+]);
+expectType<Zip<[['barney', 'fred'], [36, 40], [false, true]]>>([
+  ['barney', 36, false],
+  ['fred', 40, true],
+]);
+expectType<Zip<[['barney', 36], ['fred', 40, false]]>>([
+  ['barney', 'fred'],
+  [36, 40],
+  [undefined, false],
+]);
