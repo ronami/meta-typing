8ad3c63ac36d81140858ebf76297b159fb111bf3
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 00:25:15 +0200

    comments head/tail

--- src/head/index.d.ts
+++ src/head/index.d.ts
@@ -1,6 +1,7 @@
 // Gets the first element of array: https://lodash.com/docs/4.17.15#head.
 //
-//
-export type Head<T extends Array<any>> = T extends [any, ...Array<any>]
-  ? T['0']
-  : never;
+// If `T` is of type array and has at least one element, return it. Otherwise,
+// return `never`:
+export type Head<T extends Array<any>> =
+  // We specifically check if it extends an array with at least one item:
+  T extends [any, ...Array<any>] ? T['0'] : never;
--- src/tail/index.d.ts
+++ src/tail/index.d.ts
@@ -1,6 +1,15 @@
-export type Tail<Tuple extends Array<any>> = ((...t: Tuple) => void) extends (
-  h: any,
-  ...rest: infer R
-) => void
-  ? R
-  : never;
+// Gets all but the first element of array: https://lodash.com/docs/4.17.15#tail.
+export type Tail<T extends Array<any>> =
+  // This is a bit tricky: We define a function that gets an array of values which we assign
+  // the type of `T` (as `T` is already of type Array<any>).
+  //
+  // We then check if that function extends another function of a similar type, which should
+  // always return true. The second function takes one argument which we don't care about and
+  // and array of other values.
+  //
+  // We use the newly introduced `infer` keyword to match whatever type the array of `...rest`
+  // will be and assign its type to `R`.
+  //
+  // Since this check always returns true, we return the `infer`d type of `R` which is every
+  // value of the array but the first.
+  ((...t: T) => void) extends (h: any, ...rest: infer R) => void ? R : never;
