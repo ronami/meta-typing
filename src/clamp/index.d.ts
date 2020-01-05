3d57a8ff03b374c41da55ca764c5b6042ac1469b
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 00:47:46 +0200

    comment clamp

--- src/clamp/index.d.ts
+++ src/clamp/index.d.ts
@@ -1,10 +1,12 @@
 import { Gte, Lte } from '..';
 
-export type Clamp<A extends number, L extends number, U extends number> = Gte<
-  A,
-  U
-> extends true
-  ? U
-  : Lte<A, L> extends true
-  ? L
-  : A;
+// Clamps number within the inclusive lower and upper bounds:
+// https://lodash.com/docs/4.17.15#clamp.
+//
+// Another one which isn't recursive, yay for us.
+export type Clamp<A extends number, L extends number, U extends number> =
+  // If `A` is greater than the upper limit, return the upper limit. Otherwise,
+  // if `A` is less than the lower limit, return the lower limit.
+  //
+  // If both are `false` then `A` is within our limits and we return it:
+  Gte<A, U> extends true ? U : Lte<A, L> extends true ? L : A;
