0695664d7b0667a0be8bcd3b8c3d7fa5b20aa4dd
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-05 23:31:17 +0200

    comment add

--- src/add/index.d.ts
+++ src/add/index.d.ts
@@ -4,7 +4,19 @@ import { Inc, Dec } from '..';
 //
 // Because TypeScript's type system has no support for arithmetic,
 // the implementation uses recursion.
-export type Add<A extends number, B extends number> = {
+export type Add<
+  // Accept two numbers two add together
+  A extends number,
+  B extends number
+> = {
+  // If `B` is 0 then we're done, just return `A`
   0: A;
+  // Otherwise, call `Add` recursively while increasing `A` by 1 and decreasing
+  // `B` by one. Eventually `B` will be 0 and the recursion will terminate.
+  //
+  // For example: Add<2, 3> will first translate into another Add call that will
+  // increase `A` by one and decrease `B` by 1: Add<3, 2>. Since `B` isn't 0, we run
+  // again: Add<4, 1>, and then again: Add<5, 0>. Now that `B` is 0, the recursion
+  // terminates and returns the accumulated value of `A` which is 5.
   1: Add<Inc<A>, Dec<B>>;
 }[B extends 0 ? 0 : 1];
