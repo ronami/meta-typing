3ab1c21098045b4fd767e831811322b5b4ad4bfb
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-05 23:53:26 +0200

    add comments to lte/gte

--- src/gte/index.d.ts
+++ src/gte/index.d.ts
@@ -1,7 +1,24 @@
 import { Inc, IsNever } from '..';
 
-export type Gte<A extends number, B extends number> = {
+// Checks if `A` is greater than or equal to `B`: https://lodash.com/docs/4.17.15#gte.
+//
+// Since TypeScript's type system has no concept of arithmetic, we try incrementing both `A`
+// and `B` by 1 every time the recursion runs. The first one to equal the `never` type is
+// is the larger one.
+//
+// This is relying on how we implemented the `Inc` method: If a value is out of scope (larger
+// than 10) we return `never`. Similarily to how the `Infinity` value works in JavaScript.
+export type Gte<
+  // Accept two numbers two compare, should return `true` if `A` is bigger than or equals to
+  // `B`.
+  A extends number,
+  B extends number
+> = {
+  // If `A` is `never` then it's bigger (or equal, since we didn't check `B` yet), return
+  // `true`:
   0: true;
+  // Otherwise, if `B` is `never`, return false since it's bigger than `A`:
   1: false;
+  // None of them are `never`? Run again by increasing both by 1:
   2: Gte<Inc<A>, Inc<B>>;
 }[IsNever<A> extends true ? 0 : IsNever<B> extends true ? 1 : 2];
--- src/lte/index.d.ts
+++ src/lte/index.d.ts
@@ -1,7 +1,24 @@
 import { Inc, IsNever } from '..';
 
-export type Lte<A extends number, B extends number> = {
+// Checks if `A` is less than or equal to `B`: https://lodash.com/docs/4.17.15#lte.
+//
+// Since TypeScript's type system has no concept of arithmetic, we try decreasing both `A`
+// and `B` by 1 every time the recursion runs. The first one to equal the `never` type is
+// is the smaller one.
+//
+// This is relying on how we implemented the `Dec` method: If a value is out of scope (smaller
+// than -1) we return `never`. Similarily to how the `-Infinity` value works in JavaScript.
+export type Lte<
+  // Accept two numbers two compare, should return `true` if `A` is smaller than or equals to
+  // `B`.
+  A extends number,
+  B extends number
+> = {
+  // If `A` is `never` then it's smaller (or equal, since we didn't check `B` yet), return
+  // `true`:
   0: false;
+  // Otherwise, if `B` is `never`, return false since it's smaller than `A`:
   1: true;
+  // None of them are `never`? Run again by decreasing both by 1:
   2: Lte<Inc<A>, Inc<B>>;
 }[IsNever<A> extends true ? (IsNever<B> extends true ? 1 : 0) : 2];
