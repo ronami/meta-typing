2fb91ed9c9073a404278fbf393f266021b84f03f
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 00:41:26 +0200

    comment includes/isEqual

--- src/includes/index.d.ts
+++ src/includes/index.d.ts
@@ -1,8 +1,19 @@
 import { Tail, Head, IsEqual } from '..';
 
-export type Includes<T extends Array<any>, E> = T extends []
-  ? false
-  : {
-      0: true;
-      1: Includes<Tail<T>, E>;
-    }[IsEqual<Head<T>, E> extends true ? 0 : 1];
+// Checks if a value is an array: https://lodash.com/docs/4.17.15#includes.
+//
+export type Includes<T extends Array<any>, E> =
+  // We start by checking if the `T` array is empty. If it is, we reached the end of the
+  // recursion without finding any matching elements and return `false`:
+  T extends []
+    ? false
+    : {
+        // If it's not empty, we use the Head function to get the first element of the array
+        // and use IsEqual to check if it equals to the value of `E`.
+        //
+        // If it is, stop the recursion and return true:
+        0: true;
+        // Otherwise, keep the recursion going by passing the rest of the array (everything
+        // but the first element which we just checked):
+        1: Includes<Tail<T>, E>;
+      }[IsEqual<Head<T>, E> extends true ? 0 : 1];
--- src/isEqual/index.d.ts
+++ src/isEqual/index.d.ts
@@ -1,1 +1,8 @@
-export type IsEqual<A, B> = A extends B ? (B extends A ? true : false) : false;
+// Performs a deep comparison between two values to determine if they are equivalent:
+// https://lodash.com/docs/4.17.15#isEqual.
+//
+// For a change this isn't recursive. If `A` and `B` both extend each other, we treat them
+// as equal:
+export type IsEqual<A, B> =
+  // Return `true` only if both extend each other:
+  A extends B ? (B extends A ? true : false) : false;
