8ca4742f6148ee7d97fc2a182660d2a9a192c8cc
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 21:50:47 +0200

    comment min/max

--- src/max/index.d.ts
+++ src/max/index.d.ts
@@ -1,10 +1,22 @@
 import { Head, Gte, Tail } from '..';
 
-export type Max<T extends Array<number>, R extends number = Head<T>> = {
+// Computes the maximum value of array: https://lodash.com/docs/4.17.15#max.
+export type Max<
+  // The array to iterate over.
+  T extends Array<number>,
+  // An internal variable to track the currently maximum value. In the first
+  // call this is initialized to the first element of the array.
+  R extends number = Head<T>
+> = {
+  // Check if the input array is empty and return `never` if it is:
   0: never;
+  // Otherwise, check if the input array has only one element. If that's
+  // the case, return `R` since it's already that element:
   1: R;
+  // Then, check if the
   2: Max<Tail<T>, R>;
-  3: Max<Tail<T>, Head<Tail<T>>>;
+  //
+  3: Max<Tail<T>>;
 }[T extends []
   ? 0
   : T extends [number]
--- src/min/index.d.ts
+++ src/min/index.d.ts
@@ -1,14 +1,26 @@
-import { Head, Gte, Tail } from '..';
+import { Head, Lte, Tail } from '..';
 
-export type Min<T extends Array<number>, R extends number = Head<T>> = {
+// Computes the minimum value of array: https://lodash.com/docs/4.17.15#min.
+export type Min<
+  // The array to iterate over.
+  T extends Array<number>,
+  // An internal variable to track the currently minimum value. In the first
+  // call this is initialized to the first element of the array.
+  R extends number = Head<T>
+> = {
+  // Check if the input array is empty and return `never` if it is:
   0: never;
+  // Otherwise, check if the input array has only one element. If that's
+  // the case, return `R` since it's already that element:
   1: R;
+  // Then, check if the
   2: Min<Tail<T>, R>;
-  3: Min<Tail<T>, Head<Tail<T>>>;
+  //
+  3: Min<Tail<T>>;
 }[T extends []
   ? 0
   : T extends [number]
   ? 1
-  : Gte<R, Head<Tail<T>>> extends false
+  : Lte<R, Head<Tail<T>>> extends true
   ? 2
   : 3];
