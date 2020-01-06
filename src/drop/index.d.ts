e98bf30aa99548a3c40e8b156741df66db8b0d77
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 20:05:33 +0200

    comment drop

--- src/drop/index.d.ts
+++ src/drop/index.d.ts
@@ -1,7 +1,19 @@
 import { Dec, Tail } from '..';
 
-export type Drop<A extends Array<any>, N extends number> = {
+// Creates a slice of array with n elements dropped from the beginning:
+// https://lodash.com/docs/4.17.15#drop.
+export type Drop<
+  // The input array.
+  A extends Array<any>,
+  // The number of elements to drop from the beginning.
+  N extends number
+> = {
+  // If the input array `A` is empty, we either reached the end of our recursion
+  // or that the initial input was empty. Either way, return an empty array:
   0: [];
+  // Check if `N` is 0, which means that no more elements need to be dropped, and
+  // return the input array:
   1: A;
+  // Otherwise, run again by dropping the first element and decrementing `N` by 1:
   2: Drop<Tail<A>, Dec<N>>;
 }[A extends [] ? 0 : N extends 0 ? 1 : 2];
