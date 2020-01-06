48b05ac91f710a01587aa03ff1845f77ff4b121e
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-06 21:25:06 +0200

    comment inRange

--- src/inRange/index.d.ts
+++ src/inRange/index.d.ts
@@ -1,10 +1,17 @@
 import { Gte, Lte } from '..';
 
-export type InRange<A extends number, S extends number, E extends number> = Gte<
-  A,
-  S
-> extends true
-  ? Lte<A, E> extends true
-    ? true
-    : false
-  : false;
+// Checks if `N` is between start and up to, but not including, end:
+// https://lodash.com/docs/4.17.15#inRange.
+export type InRange<
+  // The number to check.
+  N extends number,
+  // The start of the range.
+  S extends number,
+  // The end of the range.
+  E extends number
+> =
+  // If `N` if greather than or equal to the start of the range and it's smaller
+  // than or equal to the end of the range, return true.
+  //
+  // Otherwise, it's false:
+  Gte<N, S> extends true ? (Lte<N, E> extends true ? true : false) : false;
