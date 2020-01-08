7ca2f2f7d837f0e1748de994e1fd76f5dcf0a06a
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-08 20:41:47 +0200

    comment last

--- src/last/index.d.ts
+++ src/last/index.d.ts
@@ -1,7 +1,16 @@
 import { Tail, Head } from '..';
 
-export type Last<A extends Array<any>> = {
+// Gets the last element of array: https://lodash.com/docs/4.17.15#last.
+export type Last<
+  // The array to query.
+  A extends Array<any>
+> = {
+  // If the array is empty then there is no last element and we simply return `undefined`:
   0: undefined;
+  // Next, check if the array has only 1 element in it. If that's the case, that element is
+  // the last element, simply return it as the result:
   1: Head<A>;
+  // Otherwise, then the next element isn't the last and we run the recursion again with the
+  // rest of the array:
   2: Last<Tail<A>>;
 }[A extends [] ? 0 : A extends [any] ? 1 : 2];
