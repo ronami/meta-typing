6922e18a805b2ab389112a0f750fac186efd1855
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-08 21:29:05 +0200

    comment size

--- src/size/index.d.ts
+++ src/size/index.d.ts
@@ -1,1 +1,6 @@
-export type Size<T extends Array<any>> = T['length'];
+// Gets the size of an array: https://lodash.com/docs/4.17.15#size.
+export type Size<
+  // The array to inspect.
+  T extends Array<any>
+  // Pretty straight-forward, since we know it's an array, it has a length property:
+> = T['length'];
