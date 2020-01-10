fc3ddc83abfd7b2705d405dbe44db913a094d341
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-10 12:09:23 +0200

    refactor zip

--- src/zip/index.d.ts
+++ src/zip/index.d.ts
@@ -1,21 +1,50 @@
-import { Inc, Reverse, Unshift, Tail, Head, IsEqual } from '..';
+import { Inc, Reverse, Unshift, Tail, Head, IsEqual, Cast } from '..';
 
+//
 type GetAtIndex<
+  //
   B extends Array<Array<any>>,
+  //
   C extends number,
-  R extends Array<number> = []
+  //
+  R extends Array<any> = []
 > = {
+  //
   0: Reverse<R>;
-  // @ts-ignore
-  1: GetAtIndex<Tail<B>, C, Unshift<R, Head<B>[C]>>;
+  //
+  1: Unshift<R, Head<B>[C]> extends infer G
+    ? GetAtIndex<Tail<B>, C, Cast<G, Array<any>>>
+    : never;
 }[B extends [] ? 0 : 1];
 
-type AllEqual<T extends Array<any>, E> = T extends []
-  ? true
-  : {
-      0: AllEqual<Tail<T>, E>;
-      1: false;
-    }[IsEqual<Head<T>, E> extends true ? 0 : 1];
+//
+type AllEqual<T extends Array<any>, E> = {
+  //
+  0: true;
+  //
+  1: AllEqual<Tail<T>, E>;
+  //
+  2: false;
+}[T extends [] ? 0 : IsEqual<Head<T>, E> extends true ? 1 : 2];
+
+//
+type Zipper<
+  //
+  T extends Array<Array<any>>,
+  //
+  N extends number,
+  //
+  R extends Array<Array<any>>,
+  //
+  F extends Array<any>
+> = {
+  //
+  0: [];
+  //
+  1: Reverse<R>;
+  //
+  2: Zip<T, Inc<N>, Unshift<R, F>>;
+}[T extends [] ? 0 : AllEqual<F, undefined> extends true ? 1 : 2];
 
 // Creates an array of grouped elements, the first of which contains the first elements
 // of the given arrays, the second of which contains the second elements of the given
@@ -23,20 +52,15 @@ type AllEqual<T extends Array<any>, E> = T extends []
 //
 //   type S = Zip<[['a', 'b'], [1, 2], [true, false]]>; // [['a', 1, true], ['b', 2, false]]
 //
-// @ts-ignore
 export type Zip<
   //
   T extends Array<Array<any>>,
   //
   N extends number = 0,
   //
-  R extends Array<Array<any>> = [],
+  R extends Array<Array<any>> = []
+> =
   //
-  F extends Array<any> = GetAtIndex<T, N>
-> = T extends []
-  ? []
-  : {
-      0: Reverse<R>;
-      // @ts-ignore
-      1: Zip<T, Inc<N>, Unshift<R, GetAtIndex<T, N>>>;
-    }[AllEqual<F, undefined> extends true ? 0 : 1];
+  GetAtIndex<T, N> extends infer G
+    ? Zipper<T, N, R, Cast<G, Array<any>>>
+    : never;
