fa11a78488621d004e5740ab9aa35ff414f60f4c
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-11 13:42:29 +0200

    slice example

--- src/slice/index.d.ts
+++ src/slice/index.d.ts
@@ -28,4 +28,19 @@ export type Slice<
   // we run the recursion again on the rest of the array, with the same start position (0),
   // decrease the end position by 1, and also push the current element into our accumulator:
   1: Slice<Tail<T>, S, Dec<E>, Unshift<R, Head<T>>>;
+  // For example, calling Slice<[1, 2, 3, 4], 1, 2> will first translate into:
+  // Slice<[2, 3, 4], 0, 2, []>. Notice that the first element was dropped because the start
+  // position wasn't 0 yet, and the recursion runs again on the reset of the array.
+  //
+  // Next, the recursion will look like this: Slice<[3, 4], 0, 1, [2]>. Notice that now, with
+  // the start position being 0, the first element of the array (2) was inserted into the
+  // accumulator, and the value of the end position was reduced by 1. The recursion then runs
+  // on the rest of the array.
+  //
+  // Then, it will look like this: Slice<[4], 0, 0, [3, 2]>. Again, since start position is 0
+  // and the end position isn't 0, the first element (3) is inserted into the accumulator
+  // and the recursion runs on the rest of the array and decreases the end position by 1.
+  //
+  // Finally, now that both the start and end positions are 0, the reversed accumulator is
+  // returned which results with: [2, 3].
 }[T extends [] ? 0 : S extends 0 ? (E extends 0 ? 0 : 1) : 2];
