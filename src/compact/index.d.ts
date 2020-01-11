d485589aeac178ddc29815aa9ba38936eb17e6db
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-11 12:50:31 +0200

    refactor compact

--- src/compact/index.d.ts
+++ src/compact/index.d.ts
@@ -1,4 +1,4 @@
-import { Tail, Head, Unshift, Reverse } from '..';
+import { Pull } from '..';
 
 // Creates an array with all falsey values removed. The values false, null, 0,
 // "", and undefined are falsey: https://lodash.com/docs/4.17.15#compact.
@@ -7,30 +7,11 @@ import { Tail, Head, Unshift, Reverse } from '..';
 //
 export type Compact<
   // The input array.
-  T extends Array<any>,
-  // An internal array that accumulates the results. Once the recursion ends, it
-  // be returned.
-  R extends Array<any> = []
+  T extends Array<any>
 > =
-  // If the input array is empty, return `R`. We `Reverse` the it before returning
-  // it since we keep unshifting elements into it.
-  T extends []
-    ? Reverse<R>
-    : {
-        // If `T` is a falsey value, run the recursion again with the rest of the
-        // array:
-        0: Compact<Tail<T>, R>;
-        // Otherwise, insert the new element to the accumulator array `R` and run
-        // the recursion again with the rest of the array:
-        1: Compact<Tail<T>, Unshift<R, Head<T>>>;
-      }[Head<T> extends 0
-        ? 0
-        : Head<T> extends null
-        ? 0
-        : Head<T> extends undefined
-        ? 0
-        : Head<T> extends false
-        ? 0
-        : Head<T> extends ''
-        ? 0
-        : 1];
+  // Pull removes all given values from an array. Compact is a more specific form of
+  // pull that removes falsey values from an array while pull allows us to remove any
+  // list of values.
+  //
+  // We use it here to filter falsy values from an array:
+  Pull<T, [0, null, undefined, false, '']>;
