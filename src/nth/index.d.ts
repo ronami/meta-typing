f296a8781eb7482fbd9c3edaab2ef0f4e2dd8777
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-11 11:59:54 +0200

    refactor nth

--- src/nth/index.d.ts
+++ src/nth/index.d.ts
@@ -1,5 +1,3 @@
-import { Head, Tail, Dec } from '..';
-
 // Gets the element at index n of array: https://lodash.com/docs/4.17.15#nth.
 //
 //   type S = Nth<[1, 2, 3], 1>; // 2
@@ -8,15 +6,8 @@ export type Nth<
   // The array to query.
   T extends Array<any>,
   // The index of the element to return.
-  R extends number = 0
-> = {
-  // If the input array is empty, end the recursion and return `undefined`
-  // as it means we couldn't find an element at `R` position:
-  0: undefined;
-  // Otherwise, if `R` is 0, then it means the next element in the array
-  // is the one we're looking for. Return it:
-  1: Head<T>;
-  // If `R` isn't 0 and there are still more elements in the array, run the
-  // recursion again on the rest of the array and decrease `R`'s value by 1:
-  2: Nth<Tail<T>, Dec<R>>;
-}[T extends [] ? 0 : R extends 0 ? 1 : 2];
+  N extends number = 0
+> =
+  // Pretty straight-forward: We check if `R` is a key that exists on `T`. If it is,
+  // we return its value. Otherwise, it's `undefined` that we return:
+  N extends keyof T ? T[N] : undefined;
