3d2089d8fbcf7ca1d48ea1690f5a08c58e5971bb
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-11 18:15:32 +0200

    comment chunk

--- src/chunk/index.d.ts
+++ src/chunk/index.d.ts
@@ -11,15 +11,35 @@ export type Chunk<
   T extends Array<any>,
   // The length of each chunk.
   S extends number = 1,
-  //
+  // An accumulator to gather the results.
   R extends Array<Array<any>> = []
 > = {
-  //
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   0: Reverse<R>;
+  // Otherwise, run recursively by inserting the `S` first elements from the input array (`T`)
+  // into the accumulator array, and dropping the same number of elements from the beginning
+  // of the input array (`T`).
   //
+  // Notice that we split the computation into three steps, with conditions that will always be true.
+  // This is done to trick the compiler and avoid errors of "Type instantiation is excessively
+  // deep..." from the compiler (See more: https://github.com/microsoft/TypeScript/issues/34933).
+  //
+  // Normally, the computation bellow is the same as `Chunk<Drop<T, S>, S, Drop<T, S>>`.
   1: Unshift<R, Take<T, S>> extends infer G
     ? Drop<T, S> extends infer Z
       ? Chunk<Cast<Z, Array<any>>, S, Cast<G, Array<any>>>
       : never
     : never;
+  // For example, calling Chunk<['a', 'b', 'c', 'd'], 2> will first translate into:
+  // Chunk<['c', 'd'], 2, [['a', 'b']]>. Since the input array isn't empty, the recursion ran again
+  // by dropping the first two elements from the input array, and inserting them into the accumulator
+  // array.
+  //
+  // Then again, since the input array still wasn't empty, the recursion runs with:
+  // Chunk<[], 2, [['c', 'd'], ['a', 'b']]>. The first two elements were dropped from the input array
+  // and inserted into the beginning og the accumulator.
+  //
+  // Finally, since the input array is empty, the reverse accumulator is return which results with:
+  // [['a', 'b'], ['c', 'd']].
 }[T extends [] ? 0 : 1];
