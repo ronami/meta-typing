ce3de7fed035f145be9648f1cd688512f3127f32
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-11 18:01:47 +0200

    comment intersection/difference

--- src/difference/index.d.ts
+++ src/difference/index.d.ts
@@ -20,7 +20,7 @@ export type Difference<
 
 // A helper function that takes the first array (`T`) and all other arrays (`G`).
 // It iterates over `T` and inserts every value of `T` into `R`, as long as it's
-// not in any of the arrays of `G`.
+// not included any of the arrays of `G`.
 type Diff<
   // The input array to compare against `G`.
   T extends Array<any>,
@@ -36,8 +36,26 @@ type Diff<
   // recursion again with the rest of `T`, and insert that element into the accumulator
   // array.
   1: Diff<Tail<T>, G, Unshift<R, Head<T>>>;
-  // Otherwise, if the first element of `T` is in any of the arrays in `G`, skip it
-  // by running the recursion again on the rest of `T`, without changing the accumulator
-  // at all.
+  // Otherwise, if the first element of `T` is included in any of the arrays in `G`,
+  // skip it by running the recursion again on the rest of `T`, without changing the
+  // accumulator at all.
   2: Diff<Tail<T>, G, R>;
+  // For example, calling Difference<[[1, 2, 3], [2, 3, 4]]> will first translate into:
+  // Diff<[1, 2, 3], [[2, 3, 4]], []>. Notice that `Diff` was called with the first array
+  // and all other arrays as its 2nd argument, along with an empty accumulator.
+  //
+  // Next, since `T` isn't empty, we check if the first element of `T` (1) is included
+  // in any of the arrays in `G`. Since it's not included, we run the recursion again
+  // on the rest of the input array, and insert the first element into the accumulator:
+  // Diff<[2, 3], [[2, 3, 4]], [1]>.
+  //
+  // Then, the next element of `T` is checked against the other arrays, and because it
+  // exists in one, it's not added to the accumulator: Diff<[3], [[2, 3, 4]], [1]>.
+  //
+  // Then again, the next element (3) is checked against the arrays in `G`. It is included
+  // in one of them, so it's skipped and not inserted into the accumulator:
+  // Diff<[], [[2, 3, 4]], [1]>.
+  //
+  // Finally, since the input array is empty, the reversed accumulator is return which
+  // results with: [1].
 }[T extends [] ? 0 : InEvery<G, Head<T>> extends false ? 1 : 2];
--- src/intersection/index.d.ts
+++ src/intersection/index.d.ts
@@ -5,23 +5,57 @@ import { Tail, Head, Unshift, InEvery, Reverse } from '..';
 //
 //   type S = Intersection<[[1, 2, 3], [2, 3, 4]]>; // [2, 3]
 //
-export type Intersection<T extends Array<Array<any>>> =
+export type Intersection<
   // The arrays to inspect.
-  T extends [] ? [] : Head<T> extends [] ? [] : Intersect<Head<T>, Tail<T>>;
+  T extends Array<Array<any>>
+> = {
+  // Check if the input array is empty and return an empty array.
+  //
+  // If it's not empty, check if its first element is an empty array. If that's the
+  // case, also return an empty array.
+  0: [];
+  // Otherwise, run the `Intersect` with the first array, and all other arrays.
+  1: Intersect<Head<T>, Tail<T>>;
+}[T extends [] ? 0 : Head<T> extends [] ? 0 : 1];
 
-//
+// A helper function that takes the first array (`T`) and all other arrays (`G`).
+// It iterates over `T` and inserts every value of `T` into `R`, as long as it's
+// included any of the arrays of `G`.
 type Intersect<
-  //
+  // The input array to compare against `G`.
   T extends Array<any>,
-  //
+  // The arrays to compare against `G`.
   G extends Array<Array<any>>,
-  //
+  // An accumulator to collect the results.
   R extends Array<any> = []
 > = {
-  //
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   0: Reverse<R>;
-  //
+  // If the first element of `T` is included in all of the arrays in `G`, run the
+  // recursion again with the rest of `T`, and insert that element into the accumulator
+  // array.
   1: Intersect<Tail<T>, G, Unshift<R, Head<T>>>;
-  //
+  // Otherwise, if the first element of `T` is not included in all of the arrays in `G`
+  // skip it by running the recursion again on the rest of `T`, without changing the
+  // accumulator at all.
   2: Intersect<Tail<T>, G, R>;
+  // For example, calling Intersection<[[1, 2, 3], [2, 3, 4]]> will first translate into:
+  // Intersect<[1, 2, 3], [[2, 3, 4]], []>. Notice that `Intersect` was called with the
+  // first array and all other arrays as its 2nd argument, along with an empty accumulator.
+  //
+  // Next, since `T` isn't empty, we check if the first element of `T` (1) is included
+  // in any of the arrays in `G`. Since it's not included, we run the recursion again
+  // on the rest of the input array, and skip inserting it into the accumulator:
+  // Intersect<[2, 3], [[2, 3, 4]], []>.
+  //
+  // Then, the next element of `T` is checked against the other arrays, and because it
+  // exists in all of them, it's inserted to the accumulator:
+  // Intersect<[3], [[2, 3, 4]], [2]>.
+  //
+  // Then again, the next element (3) is checked against the arrays in `G`. It is included
+  // in all of them, so it's inserted into the accumulator: Intersect<[], [[2, 3, 4]], [3, 2]>.
+  //
+  // Finally, since the input array is empty, the reversed accumulator is return which
+  // results with: [2, 3].
 }[T extends [] ? 0 : InEvery<G, Head<T>> extends true ? 1 : 2];
