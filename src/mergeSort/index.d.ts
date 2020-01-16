b6ac07e217d77b26dc61db35937386d0e8dc413b
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-16 21:41:42 +0200

    add comments for mergeSort/quickSort

--- src/mergeSort/index.d.ts
+++ src/mergeSort/index.d.ts
@@ -1,7 +1,25 @@
 import { Head, Lte, Tail, Unshift, Cast, Take, Divide, Size, Drop } from '..';
 
+// Sorts an array of number in a descending order with merge-sort algorithm.
+// https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/sorting/merge-sort.
 //
-export type MergeSort<T extends Array<any>> = {
+// type S = MergeSort<[6, 9, 7, 1, 0, 4, 3]>; // [0, 1, 3, 4, 6, 7, 9]
+//
+// Notice that the function is implemented with an object and a ternary check that accesses
+// one of it's properties:
+//
+// {
+//   0: A;
+//   1: B;
+// }[T extends H ? 0 : 1]
+//
+// This is essentially the same as writting: `T extends H ? A : B`. If this type is using
+// recursion, using the latter approach quickly results with type errors of infinite recursion
+// so the first option is used to avoid false compiler errors.
+export type MergeSort<
+  // The input array.
+  T extends Array<any>
+> = {
   //
   0: [];
   //
--- src/quickSort/index.d.ts
+++ src/quickSort/index.d.ts
@@ -10,13 +10,28 @@ import {
   Concat,
 } from '..';
 
+// Sorts an array of number in a descending order with quick-sort algorithm.
+// https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/sorting/quick-sort.
 //
+// type S = QuickSort<[6, 9, 7, 1, 0, 4, 3]>; // [0, 1, 3, 4, 6, 7, 9]
+//
+// Notice that the function is implemented with an object and a ternary check that accesses
+// one of it's properties:
+//
+// {
+//   0: A;
+//   1: B;
+// }[T extends H ? 0 : 1]
+//
+// This is essentially the same as writting: `T extends H ? A : B`. If this type is using
+// recursion, using the latter approach quickly results with type errors of infinite recursion
+// so the first option is used to avoid false compiler errors.
 export type QuickSort<
-  //
+  // The input array.
   T extends Array<number>,
-  //
+  // A local variable for the first element in `T`.
   X extends number = Head<T>,
-  //
+  // A local variable for the tail for `T`.
   Z extends Array<number> = Tail<T>
 > = {
   //
