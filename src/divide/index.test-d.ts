f11bf610de6d3ad953dc6a0205347f17b0711837
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-18 11:17:35 +0200

    add divide tests for fractions

--- src/divide/index.test-d.ts
+++ src/divide/index.test-d.ts
@@ -9,3 +9,6 @@ expectType<Divide<4, 2>>(2);
 expectType<Divide<8, 2>>(4);
 expectType<Divide<9, 3>>(3);
 expectType<Divide<8, 4>>(2);
+expectType<Divide<6, 4>>(1);
+expectType<Divide<2, 4>>(0);
+expectType<Divide<7, 3>>(2);
