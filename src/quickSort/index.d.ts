c4a8cb0390ea06b9412eaf911234cdcb22cd9366
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-20 22:07:11 +0200

    comment quick sort

--- src/quickSort/index.d.ts
+++ src/quickSort/index.d.ts
@@ -52,39 +52,46 @@ export type QuickSort<
         : never
       : never
     : never;
+  //
 }[T extends [] ? 0 : 1];
 
 // Returns an array with all the values that are smaller than or equal the pivot (`E`).
 type SmallerPart<
-  //
+  // The pivot element.
   E extends number,
-  //
+  // The array to compare its elements against the pivot.
   T extends Array<number>,
   // An accumulator to collect the results.
   R extends Array<number> = []
 > = {
-  //
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   0: Reverse<R>;
-  //
+  // Then, check if the first element of the array is less than or equal to `E`. If it
+  // is, insert it into the beginning of the accumulator and run the recursion again
+  // on the rest of the array.
   1: SmallerPart<E, Tail<T>, Unshift<R, Head<T>>>;
-  //
+  // Otherwise, skip this element and run the recursion again on the rest of the array.
   2: SmallerPart<E, Tail<T>, R>;
 }[T extends [] ? 0 : Lte<Head<T>, E> extends true ? 1 : 2];
 
 // Returns an array with all the values that are bigger than the pivot (`E`).
 type BiggerPart<
-  //
+  // The pivot element.
   E extends number,
-  //
+  // The array to compare its elements against the pivot.
   T extends Array<number>,
   // An accumulator to collect the results.
   R extends Array<number> = []
 > = {
-  //
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   0: Reverse<R>;
-  //
+  // Then, check if the first element of the array is greater than `E`. If it is, insert
+  // it into the beginning of the accumulator and run the recursion again on the rest of
+  // the array.
   1: BiggerPart<E, Tail<T>, Unshift<R, Head<T>>>;
-  //
+  // Otherwise, skip this element and run the recursion again on the rest of the array.
   2: BiggerPart<E, Tail<T>, R>;
 }[T extends []
   ? 0
