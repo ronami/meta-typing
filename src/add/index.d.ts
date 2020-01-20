1abc2d2216261cb905da699bef67ddcea62908be
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-20 21:31:07 +0200

    comment add

--- src/add/index.d.ts
+++ src/add/index.d.ts
@@ -24,14 +24,17 @@ export type Add<
   A extends number,
   B extends number
 > = {
-  // If `B` is 0 then we're done, just return `A`
+  // If `B` is 0 then we're done, just return `A`.
   0: A;
-  // Otherwise, call `Add` recursively while increasing `A` by 1 and decreasing
-  // `B` by one. Eventually, `B` will be 0 and the recursion will terminate.
+  // Otherwise, call `Add` recursively while increasing `A`'s value by 1 and decreasing
+  // `B`'s value by 1. Eventually, `B`'s value will be 0 and the recursion will terminate.
   //
-  // For example, Add<2, 3> will first translate into another Add call that will
-  // increase `A` by one and decrease `B` by 1: Add<3, 2>. Since `B` isn't 0, we run
-  // again: Add<4, 1>, and then again: Add<5, 0>. Now that `B` is 0, the recursion
-  // terminates and returns the accumulated value of `A` which is 5.
+  // For example, Add<3, 2> will first translate into another `Add` call that will
+  // increase `A`'s value by 1 and decrease `B`'s value by 1: Add<4, 1>.
+  //
+  // Then, since `B` isn't 0 (it's 1), the recursion runs again: Add<5, 0>.
+  //
+  // Finally, now that `B`'s value is 0, the recursion terminates and returns the
+  // accumulated value of `A` which is 5.
   1: Add<Inc<A>, Dec<B>>;
 }[B extends 0 ? 0 : 1];
