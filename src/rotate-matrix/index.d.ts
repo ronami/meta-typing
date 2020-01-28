2234b310a61f3166c73de9ca60235fc1f88a4cbd
Ronen Amiel <ronen.amiel@gmail.com>
2020-01-28 22:58:16 +0200

    refactor and comment matrix rotation

--- src/rotate-matrix/index.d.ts
+++ src/rotate-matrix/index.d.ts
@@ -4,37 +4,50 @@
 
 import { Zip, Reverse, Cast, Unshift, Tail, Head } from '..';
 
-//
+// Rotates a matrix counter clock-wise.
 export type RotateCCW<
-  //
+  // The input matrix. The matrix is an array of arrays and represents an image.
   T extends Array<Array<any>>
 > =
   //
   Zip<T> extends infer G ? Reverse<Cast<G, Array<any>>> : never;
 
-//
+// Rotates a matrix clock-wise.
 export type RotateCW<
-  //
-  T extends Array<Array<any>>,
-  //
-  R extends Array<Array<any>> = [],
-  //
-  C extends Array<any> = Tail<T>
+  // The input matrix. The matrix is an array of arrays and represents an image.
+  T extends Array<Array<any>>
 > =
   //
   Zip<T> extends infer G ? ReverseArray<Cast<G, Array<any>>> : never;
 
+// Helper function that takes an array of arrays and reverses each of the inner arrays.
 //
+// Notice that the function is implemented with an object and a ternary check that accesses
+// one of its properties:
+//
+// {
+//   0: A;
+//   1: B;
+// }[T extends H ? 0 : 1]
+//
+// This is the same as writing: `T extends H ? A : B`. If the condition is true then
+// `A` is returned because it's referenced by the `0` key. Otherwise it's `B` that's returned
+// since it's referenced by the `1` key.
+//
+// TypScript's type system doesn't support recursive types and the above example is a way
+// of going around it. Please note that it's not something TypeScript officially supports:
+// https://github.com/microsoft/TypeScript/issues/26223#issuecomment-513187373.
 type ReverseArray<
-  //
+  // The input array.
   T extends Array<Array<any>>,
-  //
+  // An accumulator to collect the results.
   R extends Array<Array<any>> = []
 > = {
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
+  finish: Reverse<R>;
   //
-  0: Reverse<R>;
-  //
-  1: Unshift<R, Reverse<Head<T>>> extends infer G
+  next: Unshift<R, Reverse<Head<T>>> extends infer G
     ? ReverseArray<Tail<T>, Cast<G, Array<any>>>
     : never;
-}[T extends [] ? 0 : 1];
+}[T extends [] ? 'finish' : 'next'];
