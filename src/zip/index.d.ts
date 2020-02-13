75427920f2ec79c1cb50da58ff7e60e4f2fb767e
Ronen Amiel <ronen.amiel@gmail.com>
2020-02-13 20:15:24 +0200

    comment zip

--- src/zip/index.d.ts
+++ src/zip/index.d.ts
@@ -1,4 +1,4 @@
-import { Reverse, Unshift, Tail, Head, Cast, IsNever } from '..';
+import { Reverse, Unshift, Tail, Head, Cast } from '..';
 
 // Creates an array of grouped elements, the first of which contains the first elements
 // of the given arrays, the second of which contains the second elements of the given
@@ -8,21 +8,58 @@ import { Reverse, Unshift, Tail, Head, Cast, IsNever } from '..';
 //
 // This type uses recursive (and not officially supported) type alias, see more:
 // https://github.com/microsoft/TypeScript/issues/26223#issuecomment-513187373.
-export type Zip<T extends Array<Array<any>>, R extends Array<any> = []> = {
+export type Zip<
+  // The input array.
+  T extends Array<Array<any>>,
+  // An accumulator to collect the results.
+  R extends Array<any> = []
+> = {
+  // If the input array is empty or if its first element if an empty array, then return the
+  // accumulator array. We're relying that each array in the input array has the same length
+  // (it won't work otherwise).
+  //
+  // We reverse the accumulator since we add elements into it in a reversed order.
   finish: Reverse<R>;
+  // Otherwise, run the recursion again on the input array after cutting the first element
+  // of each array. Then, insert an array with the first element of each array into the
+  // beginning of thhe accumulator.
+  //
+  // Computation is split into multiple steps with `infer`, see more:
+  // https://github.com/pirix-gh/medium/blob/master/types-curry-ramda/src/index.ts#L17.
   next: AllTails<T> extends infer G
     ? Zip<Cast<G, Array<any>>, Unshift<R, AllHeads<T>>>
     : never;
 }[T extends [] ? 'finish' : T extends [[], ...Array<any>] ? 'finish' : 'next'];
 
-type Default<V, D> = IsNever<V> extends true ? D : V;
-
-type AllHeads<T extends Array<Array<any>>, R extends Array<any> = []> = {
+// A helper function that maps over an array and returns a new array with the first element
+// of each array.
+type AllHeads<
+  // The input array.
+  T extends Array<Array<any>>,
+  // An accumulator to collect the results.
+  R extends Array<any> = []
+> = {
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   finish: Reverse<R>;
-  next: AllHeads<Tail<T>, Unshift<R, Default<Head<Head<T>>, undefined>>>;
+  // Otherwise, run the recursion again on the rest of the array, and insert the first
+  // element of the first array into the beginning of the accumulator.
+  next: AllHeads<Tail<T>, Unshift<R, Head<Head<T>>>>;
 }[T extends [] ? 'finish' : 'next'];
 
-type AllTails<T extends Array<Array<any>>, R extends Array<any> = []> = {
+// A helper function that maps over an array and returns a new array with every element
+// but the first element of each array.
+type AllTails<
+  // The input array.
+  T extends Array<Array<any>>,
+  // An accumulator to collect the results.
+  R extends Array<any> = []
+> = {
+  // If the input array is empty, return the accumulator array. We reverse it first
+  // since we add elements into it in a reversed order.
   finish: Reverse<R>;
+  // Otherwise, run the recursion again on the rest of the array, and insert the tail
+  // (every element but the first element) of the first array into the beginning of the
+  // accumulator.
   next: AllTails<Tail<T>, Unshift<R, Tail<Head<T>>>>;
 }[T extends [] ? 'finish' : 'next'];
