6b759dd666be2c03d85b3523cb763f1bf8698a8b
GBT <gbt3101@gmail.com>
2020-08-10 16:37:17 +0300

    Fixing, last commit was multiply by mistake.

--- src/power/index.d.ts
+++ src/power/index.d.ts
@@ -1,41 +1,32 @@
-import { Add, Dec, Cast } from '..';
+import { Multiply, Dec, Cast } from '..';
 
-// Multiply two numbers: https://lodash.com/docs/4.17.15#multiply.
+// Power two numbers: https://lodash.com/docs/4.17.15#power.
 //
-type S = Multiply<2, 3>; // 6
+type S = Power<2, 3>; // 8
 //
 // This type uses recursive (and not officially supported) type alias, see more:
 // https://github.com/microsoft/TypeScript/issues/26223#issuecomment-513187373.
-export type Multiply<
+export type Power<
   // The first number in a multiplication.
   A extends number,
   // The second number in a multiplication.
   B extends number
   > = {
-  // If any of the numbers is 0, return 0:
+  // If A is 0: return 0
+  // If B is 0: return 1
   zero: 0;
-  // Next, check if any of them are equal to 1 and return the other number:
-  'one-b': A;
-  'one-a': B;
+  one: 1;
   // If they're both not 1s or 0s, we call the recursion again, like this:
   //
-  //   Add<A, Multiply<A, Dec<B>>>
+  //   Multiply<A, Power<A, Dec<B>>>
   //
-  // We add the value of the first number (A) to the result of calling Multiply
-  // again in which we decrease B's value by 1. This means that we call Add<A, A>
+  // We multiply the value of the first number (A) to the result of calling Multiply
+  // again in which we decrease B's value by 1. This means that we call Multiply<A, A>
   // B times.
   //
   // Computation is split into multiple steps with `infer`, see more:
   // https://github.com/pirix-gh/medium/blob/master/types-curry-ramda/src/index.ts#L17.
-  next: Multiply<A, Dec<B>> extends infer G // Assign result to `G`
-    ? Add<A, Cast<G, number>>
+  next: Power<A, Dec<B>> extends infer G // Assign result to `G`
+    ? Multiply<A, Cast<G, number>>
     : never;
-}[A extends 0
-  ? 'zero'
-  : B extends 0
-    ? 'zero'
-    : B extends 1
-      ? 'one-b'
-      : A extends 1
-        ? 'one-a'
-        : 'next'];
+}[B extends 0 ? 'one' : A extends 0 ? 'zero' : A extends 1 ? 'one' : 'next'];
--- src/power/index.test-d.ts
+++ src/power/index.test-d.ts
@@ -1,11 +1,12 @@
 import { expectType } from 'tsd';
-import { Multiply } from '.';
+import { Power } from '.';
 
-expectType<Multiply<0, 3>>(0);
-expectType<Multiply<4, 0>>(0);
-expectType<Multiply<1, 1>>(1);
-expectType<Multiply<1, 5>>(5);
-expectType<Multiply<4, 1>>(4);
-expectType<Multiply<3, 3>>(9);
-expectType<Multiply<2, 4>>(8);
-expectType<Multiply<3, 5>>(this as never);
+expectType<Power<2, 3>>(8);
+expectType<Power<3, 2>>(9);
+expectType<Power<0, 0>>(1);
+expectType<Power<0, 3>>(0);
+expectType<Power<4, 0>>(1);
+expectType<Power<1, 1>>(1);
+expectType<Power<1, 5>>(1);
+expectType<Power<4, 1>>(4);
+expectType<Power<3, 5>>(this as never);
